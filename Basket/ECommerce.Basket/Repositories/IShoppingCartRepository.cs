b4420eefa0e0c516f7e10c1c9aede31b7fe3dc5b
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-11 22:04:34 +0330

    add IShoppingCartRepository

---
+++ Basket/ECommerce.Basket/Repositories/IShoppingCartRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Basket.Repositories;
+
+public interface IShoppingCartRepository
+{
+    Task PutAsync(ShoppingCart cart);
+    Task<ShoppingCart?> FindByUserIdOrDefaultAsync(long userId);
+    Task<bool> RemoveAsync(long userId);
+}
