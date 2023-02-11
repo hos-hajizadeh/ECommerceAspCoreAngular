081343a297e1ec4ff9bebe017e66ab4bba573005
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-11 21:44:17 +0330

    add  ShoppingCart and ShoppingCartItem domains

---
+++ Basket/ECommerce.Basket/ShoppingCart.cs
@@ -0,0 +1,44 @@
+﻿namespace ECommerce.Basket;
+
+public class ShoppingCart
+{
+    private readonly long _userId;
+    private readonly HashSet<ShoppingCartItem> _items = new();
+
+    public ShoppingCart(long userId)
+    {
+        _userId = userId;
+    }
+
+    public void Add(long productId, int quantity = 1)
+    {
+        var existsItem = _items.FirstOrDefault(c => c.IsEqualsProduct(productId));
+        if (existsItem != null)
+            existsItem.IncreaseQuantity(quantity);
+        else
+        {
+            var item = new ShoppingCartItem(productId, quantity);
+            _items.Add(item);
+        }
+    }
+
+    public void Remove(long productId)
+    {
+        _items.RemoveWhere(c => c.IsEqualsProduct(productId));
+    }
+
+    public void Remove(ShoppingCartItem cartItem)
+    {
+        _items.Remove(cartItem);
+    }
+
+    public void Clear()
+    {
+        _items.Clear();
+    }
+
+    public int? GetProductQuantity(long productId)
+    {
+        return _items.FirstOrDefault(c => c.IsEqualsProduct(productId))?.Quantity;
+    }
+}
---
+++ Basket/ECommerce.Basket/ShoppingCartItem.cs
@@ -0,0 +1,26 @@
+﻿namespace ECommerce.Basket;
+
+public class ShoppingCartItem
+{
+    private readonly long _productId;
+    private int _quantity;
+
+    public ShoppingCartItem(long productId)
+    {
+        _productId = productId;
+        _quantity = 1;
+    }
+
+    public ShoppingCartItem(long productId, int quantity)
+    {
+        _productId = productId;
+        _quantity = quantity;
+    }
+
+    internal int Quantity => _quantity;
+    internal void IncreaseQuantity(int quantity = 1) => _quantity += quantity;
+
+    internal bool IsEqualsProduct(long productId) => _productId == productId;
+
+    internal bool IsEqualsProduct(ShoppingCartItem cartItem) => _productId == cartItem._productId;
+}
