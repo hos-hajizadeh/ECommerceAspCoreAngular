ad04132fc30c6202622ba1992c9c5bb16e483026
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 17:49:26 +0330

    add ShoppingCartRepositoryTest

---
+++ Test/ECommerce.Basket.Test/Data/BaseEfRepoTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Basket.Data.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Basket.Test.Data
+{
+    public abstract class BaseEfRepoTestFixture
+    {
+        protected BasketContext _dbContext;
+
+        protected BaseEfRepoTestFixture()
+        {
+            var collection = new ServiceCollection();
+            collection.AddEntityFrameworkInMemoryDatabase();
+
+            collection.AddDbContext<BasketContext>((sp, options) =>
+            {
+                options.UseInMemoryDatabase("TestDb").UseInternalServiceProvider(sp);
+            });
+
+            var serviceProvider =collection .BuildServiceProvider();
+
+            _dbContext = serviceProvider.GetService<BasketContext>()!;
+        }
+    }
+}
---
+++ Test/ECommerce.Basket.Test/Data/ShoppingCartRepositoryTest.cs
@@ -0,0 +1,62 @@
+﻿using ECommerce.Basket.Data.Repositories;
+
+namespace ECommerce.Basket.Test.Data;
+
+public class ShoppingCartRepositoryTest : BaseEfRepoTestFixture
+{
+    private const long UserId = 10;
+    private const long ProductId = 10;
+    private const int Quantity = 2;
+
+    [Fact]
+    public async Task Given_InvalidUserId_When_FindByUserIdOrDefaultAsync_Then_ReturnsNullAsResult()
+    {
+        var shoppingCartRepository = new ShoppingCartRepository(_dbContext);
+        var shoppingCart = await shoppingCartRepository.FindByUserIdOrDefaultAsync(0);
+        Assert.Null(shoppingCart);
+    }
+
+    [Fact]
+    public async Task Given_ValidData_When_PutAsync_Then_SuccessfullyInsertShoppingCart()
+    {
+        var shoppingCartRepository = new ShoppingCartRepository(_dbContext);
+        var cart = new ShoppingCart(UserId);
+        cart.Add(ProductId, Quantity);
+        
+        await shoppingCartRepository.PutAsync(cart);
+        var shoppingCart = await shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+
+        Assert.NotNull(shoppingCart);
+        Assert.Equal(Quantity, shoppingCart.GetProductQuantity(ProductId));
+    }
+
+    [Fact]
+    public async Task Given_ValidData_When_PutAsync_WithExistShoppingCart_Then_SuccessfullyUpdateShoppingCart()
+    {
+        var shoppingCartRepository = new ShoppingCartRepository(_dbContext);
+
+        var cart = new ShoppingCart(UserId);
+        cart.Add(ProductId, Quantity);
+        await shoppingCartRepository.PutAsync(cart);
+        cart.Add(ProductId, Quantity);
+        await shoppingCartRepository.PutAsync(cart);
+
+        var shoppingCart = await shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+        Assert.NotNull(shoppingCart);
+        Assert.Equal(Quantity * 2, shoppingCart.GetProductQuantity(ProductId));
+    }
+
+    [Fact]
+    public async Task Given_ValidData_When_RemoveAsync_Then_SuccessfullyDeleteShoppingCart()
+    {
+        var shoppingCartRepository = new ShoppingCartRepository(_dbContext);
+
+        await shoppingCartRepository.PutAsync(new ShoppingCart(UserId));
+        var shoppingCart = await shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+        Assert.NotNull(shoppingCart);
+
+        await shoppingCartRepository.RemoveAsync(UserId);
+        shoppingCart = await shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+        Assert.Null(shoppingCart);
+    }
+}
