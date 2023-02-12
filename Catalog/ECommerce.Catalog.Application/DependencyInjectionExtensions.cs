4f16ddb261863190ca63af18540abe2c0051b210
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 19:35:31 +0330

    add DependencyInjectionExtensions

---
+++ Catalog/ECommerce.Catalog.Application/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Catalog.Application
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddCatalogApplicationServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMediatR(typeof(DependencyInjectionExtensions));
+            return services;
+        }
+    }
+}
