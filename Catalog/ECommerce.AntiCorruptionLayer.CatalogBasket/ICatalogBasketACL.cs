af7524fa1b00158a5aa8b8385c99d351c0a1e660
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 19:54:05 +0330

    add AntiCorruptionLayer.CatalogBasket

---
+++ Catalog/ECommerce.AntiCorruptionLayer.CatalogBasket/ICatalogBasketACL.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.AntiCorruptionLayer.CatalogBasket;
+
+public interface ICatalogBasketACL
+{
+    public Task<ProductOverviewDto> GetProductOverviewAsync(long productId);
+    public Task<IEnumerable<ProductOverviewDto>> GetProductOverviewsAsync(IEnumerable<long> productIds);
+}
---
+++ Catalog/ECommerce.AntiCorruptionLayer.CatalogBasket/ProductOverviewDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.AntiCorruptionLayer.CatalogBasket;
+
+public class ProductOverviewDto
+{
+    public long Id { get; set; }
+
+    public string? Name { get; set; }
+}
