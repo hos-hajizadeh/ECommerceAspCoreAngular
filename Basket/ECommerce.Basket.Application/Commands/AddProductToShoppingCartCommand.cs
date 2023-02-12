35affe4752d879b1a35e0d42a23c7b4fa1312d27
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 18:23:52 +0330

    add AddProductToShoppingCartCommand

---
+++ Basket/ECommerce.Basket.Application/Commands/AddProductToShoppingCartCommand.cs
@@ -0,0 +1,43 @@
+using ECommerce.Basket.Repositories;
+using ECommerce.Share.Abstractions;
+using ECommerce.Share.Abstractions.CQRS;
+
+namespace ECommerce.Basket.Application.Commands;
+
+public class AddProductToShoppingCartCommand : ICommand<bool>
+{
+    public long ProductId { get; set; }
+    public int Quantity { get; set; }
+
+    public class Handler : ICommandHandler<AddProductToShoppingCartCommand, bool>
+    {
+        private readonly IWorkContext _workContext;
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+
+        public Handler(IWorkContext workContext, IShoppingCartRepository shoppingCartRepository)
+        {
+            _workContext = workContext;
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+
+        public async Task<bool> Handle(AddProductToShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            var userId = _workContext.GetCurrentUserId();
+
+            var card = await _shoppingCartRepository.FindByUserIdOrDefaultAsync(userId);
+            if (card is null)
+            {
+                card = new ShoppingCart(userId);
+                card.Add(request.ProductId, request.Quantity);
+            }
+            else
+            {
+                card.Add(request.ProductId, request.Quantity);
+            }
+
+            await _shoppingCartRepository.PutAsync(card);
+
+            return true;
+        }
+    }
+}
---
+++ Test/ECommerce.Basket.Test/Applications/AddProductToShoppingCartTest.cs
@@ -0,0 +1,42 @@
+using ECommerce.Basket.Application.Commands;
+
+namespace ECommerce.Basket.Test.Applications;
+
+public class AddProductToShoppingCartTest : BaseHandlersTestFixture
+{
+    [Fact]
+    public async void Given_ValidData_Then_SuccessfullyInsertShoppingCart()
+    {
+        await _mediator.Send(new AddProductToShoppingCartCommand()
+        {
+            ProductId = ProductId,
+            Quantity = Quantity
+        });
+
+        var shoppingCart = await _shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+        Assert.NotNull(shoppingCart);
+        Assert.Equal(Quantity, shoppingCart.GetProductQuantity(ProductId));
+    }
+
+    [Fact]
+    public async void Given_ValidData_When_WithExistShoppingCart_Then_SuccessfullyUpdateShoppingCart()
+    {
+        await _mediator.Send(new AddProductToShoppingCartCommand()
+        {
+            ProductId = ProductId,
+            Quantity = Quantity
+        });
+
+        await _mediator.Send(new AddProductToShoppingCartCommand()
+        {
+            ProductId = ProductId,
+            Quantity = Quantity
+        });
+
+        var shoppingCart = await _shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+        Assert.NotNull(shoppingCart);
+        Assert.Equal(Quantity * 2, shoppingCart.GetProductQuantity(ProductId));
+    }
+    
+    
+}
