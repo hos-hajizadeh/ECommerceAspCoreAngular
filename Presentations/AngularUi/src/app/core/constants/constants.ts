b40f7209147f88a08ad49c62737c2e58077b5a20
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 22:30:46 +0330

    add shoppingCartBaseUrl

--- Presentations/AngularUi/src/app/core/constants/constants.ts
+++ Presentations/AngularUi/src/app/core/constants/constants.ts
@@ -1,3 +1,4 @@
 export class Constants {
     public static readonly productBaseUrl ='http://localhost:5258/Product';
+    public static readonly shoppingCartBaseUrl ='http://localhost:5258/ShoppingCart';
 }
