201671c26c06279f032ea4aad3c3c0addc65ce65
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 19:19:18 +0330

    add DomainException

---
+++ Core/ECommerce.Share/Exceptions/DomainException.cs
@@ -0,0 +1,17 @@
+namespace ECommerce.Share.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException()
+        {
+        }
+
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
