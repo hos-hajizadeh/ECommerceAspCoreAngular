fcb3d9bd5e4395ff32ed4d8b7e030adc0a44de88
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 18:56:15 +0330

    add ClearShoppingCartCommand

---
+++ Basket/ECommerce.Basket.Application/Commands/ClearShoppingCartCommand.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.Basket.Repositories;
+
+namespace ECommerce.Basket.Application.Commands;
+
+public class ClearShoppingCartCommand : ICommand<bool>
+{
+    public static ClearShoppingCartCommand Default = new();
+
+    public class Handler : ICommandHandler<ClearShoppingCartCommand, bool>
+    {
+        private readonly IWorkContext _workContext;
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+
+        public Handler(IWorkContext workContext, IShoppingCartRepository shoppingCartRepository)
+        {
+            _workContext = workContext;
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+
+        public async Task<bool> Handle(ClearShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            var userId = _workContext.GetCurrentUserId();
+            var card = await _shoppingCartRepository.FindByUserIdOrDefaultAsync(userId);
+            if (card is null)
+                return false;
+
+            card.Clear();
+            await _shoppingCartRepository.PutAsync(card);
+            return true;
+        }
+    }
+}
---
+++ Test/ECommerce.Basket.Test/Applications/ClearShoppingCartTest.cs
@@ -0,0 +1,26 @@
+using ECommerce.Basket.Application.Commands;
+
+namespace ECommerce.Basket.Test.Applications;
+
+public class ClearShoppingCartTest : BaseHandlersTestFixture
+{
+    [Fact]
+    public async void Given_ValidData_Then_SuccessfullyClearShoppingCart()
+    {
+        await _mediator.Send(new AddProductToShoppingCartCommand()
+        {
+            ProductId = ProductId,
+            Quantity = Quantity
+        });
+
+        var shoppingCart = await _shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+        Assert.NotNull(shoppingCart);
+        Assert.Equal(Quantity, shoppingCart.GetProductQuantity(ProductId));
+
+        await _mediator.Send(ClearShoppingCartCommand.Default);
+
+        shoppingCart = await _shoppingCartRepository.FindByUserIdOrDefaultAsync(UserId);
+        Assert.NotNull(shoppingCart);
+        Assert.Null(shoppingCart.GetProductQuantity(ProductId));
+    }
+}
