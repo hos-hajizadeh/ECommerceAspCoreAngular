c3f8434026c18065100629b4b2a8232eed0b6f02
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 20:38:19 +0330

    update GetUserShoppingCartQueryHandler, use ICatalogBasketACL to fill ProductOverview model

--- Basket/ECommerce.Basket.Data/QueryHandlers/GetUserShoppingCartQueryHandler.cs
+++ Basket/ECommerce.Basket.Data/QueryHandlers/GetUserShoppingCartQueryHandler.cs
@@ -1,9 +1,12 @@
-﻿using ECommerce.Basket.Application.Dtos;
+﻿using ECommerce.AntiCorruptionLayer.CatalogBasket;
+using ECommerce.Basket.Application.Dtos;
 using ECommerce.Basket.Application.Queries;
+using ECommerce.Basket.Data.Entities;
 using ECommerce.Basket.Data.Persistence.DbContexts;
 using ECommerce.Share.Abstractions;
 using ECommerce.Share.Abstractions.CQRS;
 using Microsoft.EntityFrameworkCore;
+using ProductOverviewDto = ECommerce.AntiCorruptionLayer.CatalogBasket.ProductOverviewDto;
 
 namespace ECommerce.Basket.Data.QueryHandlers;
 
@@ -11,11 +14,14 @@ public class GetUserShoppingCartQueryHandler : IQueryHandler<GetUserShoppingCart
 {
     private readonly IWorkContext _workContext;
     private readonly BasketContext _basketContext;
+    private readonly ICatalogBasketACL _catalogBasketACL;
 
-    public GetUserShoppingCartQueryHandler(IWorkContext workContext, BasketContext basketContext)
+    public GetUserShoppingCartQueryHandler(IWorkContext workContext, BasketContext basketContext,
+        ICatalogBasketACL catalogBasketACL)
     {
         _workContext = workContext;
         _basketContext = basketContext;
+        _catalogBasketACL = catalogBasketACL;
     }
 
     public async Task<UserShoppingCartDto> Handle(GetUserShoppingCartQuery request, CancellationToken cancellationToken)
@@ -28,12 +34,18 @@ public class GetUserShoppingCartQueryHandler : IQueryHandler<GetUserShoppingCart
         if (card == null)
             return new UserShoppingCartDto();
 
-        var shoppingCartItemDtos = card.Items.Select(entity => new ShoppingCartItemDto
-        {
-            Id = entity.Id,
-            Quantity = entity.Quantity,
-            ProductOverview = null //todo:
-        }).ToList();
+        var productOverviews = await GetProductOverviewDtosAsync(card);
+        var shoppingCartItemDtos = card.Items.Join(productOverviews, i => i.ProductId, i => i.Id,
+            (entity, overviewDto) => new ShoppingCartItemDto
+            {
+                Id = entity.Id,
+                Quantity = entity.Quantity,
+                ProductOverview = new Application.Dtos.ProductOverviewDto
+                {
+                    Id = overviewDto.Id,
+                    Name = overviewDto.Name
+                }
+            }).ToList();
 
         var userShoppingCartDto = new UserShoppingCartDto
         {
@@ -42,5 +54,12 @@ public class GetUserShoppingCartQueryHandler : IQueryHandler<GetUserShoppingCart
         };
 
         return userShoppingCartDto;
+    }
+
+    private async Task<IEnumerable<ProductOverviewDto>> GetProductOverviewDtosAsync(ShoppingCartEntity card)
+    {
+        var productsIds = card.Items.Select(i => i.ProductId).Distinct().ToList();
+        var productOverviews = await _catalogBasketACL.GetProductOverviewsAsync(productsIds);
+        return productOverviews;
     }
 }
