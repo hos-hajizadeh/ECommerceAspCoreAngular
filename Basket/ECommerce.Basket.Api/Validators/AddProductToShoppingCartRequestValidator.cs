b2c4760ea1f36606cfedf3595619c655ea2bb7cc
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 20:33:07 +0330

    add AddProductToShoppingCartRequestValidator

---
+++ Basket/ECommerce.Basket.Api/Validators/AddProductToShoppingCartRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Basket.Api.Models;
+using FluentValidation;
+
+namespace ECommerce.Basket.Api.Validators;
+
+public class AddProductToShoppingCartRequestValidator : AbstractValidator<AddProductToShoppingCartRequest>
+{
+    public AddProductToShoppingCartRequestValidator()
+    {
+        RuleFor(x => x.ProductId).GreaterThan(0);
+        RuleFor(x => x.Quantity).GreaterThan(0);
+    }
+}
