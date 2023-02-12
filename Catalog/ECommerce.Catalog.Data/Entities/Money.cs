2ef9797672ad2cb1bebc5b9696975bbe89569c6c
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 19:38:14 +0330

    add Catalog Entities

---
+++ Catalog/ECommerce.Catalog.Data/Entities/Money.cs
@@ -0,0 +1,7 @@
+namespace ECommerce.Catalog.Data.Entities;
+
+public class Money
+{
+    public decimal Amount { get; set; }
+    public string? Currency { get; set; }
+}
---
+++ Catalog/ECommerce.Catalog.Data/Entities/ProductEntity.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Share.Abstractions;
+
+namespace ECommerce.Catalog.Data.Entities;
+
+public class ProductEntity : IDbEntity
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public Money? Price { get; set; }
+    public string? Description { get; set; }
+}
