cf4017a8eba15a0c8caa73d32b1afc2891d77300
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 20:30:13 +0330

    add ValidationFilter

---
+++ Core/ECommerce.Web.Framework/Mvc/Filters/ValidationFilter.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ECommerce.Web.Framework.Mvc.Filters;
+
+public class ValidationFilter : IAsyncActionFilter
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public ValidationFilter(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        foreach (var (_, value) in context.ActionArguments)
+        {
+            if (value is null)
+                continue;
+
+            var result = await ValidateAsync(value);
+            if (result?.IsValid != false)
+                continue;
+
+            context.Result = new BadRequestResult();
+            return;
+        }
+
+        await next();
+    }
+
+    private async Task<ValidationResult?> ValidateAsync(object value)
+    {
+        var validator = GetValidator(value.GetType());
+        if (validator == null)
+            return null;
+
+        var context = new ValidationContext<object>(value);
+        return await validator.ValidateAsync(context);
+    }
+
+    private IValidator? GetValidator(Type targetType)
+    {
+        var validatorType = typeof(IValidator<>).MakeGenericType(targetType);
+        var validator = (IValidator?)_serviceProvider.GetService(validatorType);
+        return validator;
+    }
+}
