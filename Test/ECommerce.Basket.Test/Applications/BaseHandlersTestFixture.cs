294888412e4a3c17175cc2cfa72c53ed7b6612d4
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 18:23:31 +0330

    add BaseHandlersTestFixture

---
+++ Test/ECommerce.Basket.Test/Applications/BaseHandlersTestFixture.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.Basket.Data.Persistence.DbContexts;
+using ECommerce.Basket.Data.Repositories;
+using ECommerce.Basket.Repositories;
+using ECommerce.Share.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace ECommerce.Basket.Test.Applications
+{
+    public abstract class BaseHandlersTestFixture
+    {
+        protected const int UserId = 10;
+        protected const long ProductId = 10;
+        protected const int Quantity = 2;
+        
+        protected Mock<IWorkContext> _mockWorkContext = new();
+        protected IMediator _mediator;
+        protected ServiceProvider _serviceProvider;
+        protected IShoppingCartRepository _shoppingCartRepository;
+  
+        protected BaseHandlersTestFixture()
+        {
+            _mockWorkContext.Setup(x => x.GetCurrentUserId()).Returns(UserId);
+
+            var services = new ServiceCollection();
+            services.AddEntityFrameworkInMemoryDatabase();
+            services.AddMediatR(typeof(Application.DependencyInjectionExtensions));
+            services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+            services.AddSingleton(_mockWorkContext.Object);
+
+            services.AddDbContext<BasketContext>((sp, options) =>
+            {
+                options.UseInMemoryDatabase("TestDb").UseInternalServiceProvider(sp);
+            });
+
+            _serviceProvider = services.BuildServiceProvider();
+
+            _mediator = _serviceProvider.GetService<IMediator>()!;
+            _shoppingCartRepository = _serviceProvider.GetService<IShoppingCartRepository>()!;
+        }
+    }
+}
