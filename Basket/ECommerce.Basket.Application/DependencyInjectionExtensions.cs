3f262759c1bb2550d0e0d492dbd408cf827fa128
Hossein Hajizadeh <hos.hajizadeh@gmail.com>
2023-02-12 17:53:07 +0330

    add DependencyInjectionExtensions

---
+++ Basket/ECommerce.Basket.Application/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Basket.Application
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddBasketApplicationServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMediatR(typeof(DependencyInjectionExtensions));
+            return services;
+        }
+    }
+}
