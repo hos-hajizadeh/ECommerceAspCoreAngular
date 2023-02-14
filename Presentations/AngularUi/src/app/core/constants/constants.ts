895274d035dda3b7685d920cd41edd084c79b4c5
Hajizadeh-LP <hos.hajizadeh@gmail.com>
2023-02-14 08:30:20 +0330

    update configs for for docker

--- Presentations/AngularUi/src/app/core/constants/constants.ts
+++ Presentations/AngularUi/src/app/core/constants/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static readonly productBaseUrl ='http://localhost:5258/Product';
-    public static readonly shoppingCartBaseUrl ='http://localhost:5069/ShoppingCart';
-    public static readonly purchaseBaseUrl ='http://localhost:5069/ShoppingCart/Purchase';
+    public static readonly productBaseUrl ='http://95.211.208.47:802/Product';
+    public static readonly shoppingCartBaseUrl ='http://95.211.208.47:801/ShoppingCart';
+    public static readonly purchaseBaseUrl ='http://95.211.208.47:801/ShoppingCart/Purchase';
 }
